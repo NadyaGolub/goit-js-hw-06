e8d773a6bdde17bd94ebcf5f5815a8520ec6a281
NadyaGolub <nadyagolub@ukr.net>
2022-12-08 17:35:56 +0200

    Завдання 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector(`ul#ingredients`);
+
+const elements = ingredients.map(option => {
+  const li = document.createElement(`li`);
+  li.textContent = option;
+  li.classList.add(`item`);
+  return li;
+});
+
+console.log(elements);
+list.append(...elements);
