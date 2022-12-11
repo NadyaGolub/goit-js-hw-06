18cc04cb103d98132696b0180563a06827279d12
NadyaGolub <nadyagolub@ukr.net>
2022-12-11 16:59:23 +0200

    Завдання 4

--- js/task-04.js
+++ js/task-04.js
@@ -1,4 +1,21 @@
-const decrement = document.querySelector(`#counter`);
-console.log(decrement);
+const button = {
+  subtract: document.querySelector("[data-action='increment']"),
+  add: document.querySelector("[data-action='decrement']"),
+  sum: document.querySelector("#value"),
+};
 
+let counterValue = 0;
+const increment = () => {
+  counterValue += 1;
 
+ button.sum.textContent = counterValue;
+};
+
+const decrement = () => {
+  counterValue -= 1;
+
+  button.sum.textContent = counterValue;
+};
+
+button.subtract.addEventListener("click", increment);
+button.add.addEventListener("click", decrement);
