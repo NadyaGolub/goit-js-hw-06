264220f4a88364674d99074c374a9022e8e0a61a
NadyaGolub <nadyagolub@ukr.net>
2022-12-14 16:12:43 +0200

    Завдання 1 (після зауважень)

--- js/task-01.js
+++ js/task-01.js
@@ -1,29 +1,14 @@
-const totalItem = document.querySelectorAll(".item");
-console.log(`Number of categories: ${ totalItem.length }`);
+const totalItems = document.querySelectorAll(".item");
+console.log(`Number of categories: ${ totalItems.length }`);
 
-const animals = document.querySelector(`h2`);
+totalItems.forEach((totalItem) => {
+  
+    const category = totalItem.querySelector(`h2`);
 
-console.log(`Category: ${animals.textContent}`);
+    const categoryLength = totalItem.querySelectorAll(`li`).length;
 
-const animalsEl = document.querySelectorAll(`ul`)[1];
+    console.log(`Category: ${category.textContent}`);
 
-const animalsElements = animalsEl.querySelectorAll(`li`);
-console.log(`Elements: ${animalsElements.length}`);
 
-const products = document.querySelectorAll(`h2`)[1];
-
-console.log(`Category: ${products.textContent}`);
-const productsEl = document.querySelectorAll(`ul`)[2];
-
-const productsElements = productsEl.querySelectorAll(`li`);
-console.log(`Elements: ${productsElements.length}`);
-
-const technologies = document.querySelectorAll(`h2`)[2];
-
-console.log(`Category: ${technologies.textContent}`);
-
-const technologiesEl = document.querySelectorAll(`ul`)[3];
-
-const technologiesElements = technologiesEl.querySelectorAll(`li`);
-console.log(`Elements: ${technologiesElements.length}`);
-
+    console.log(`Elements: ${categoryLength}`);
+});
