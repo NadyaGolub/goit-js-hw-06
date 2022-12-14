b8b16a8dbd02ca0e7fb07eb36a7e3ca3c7dec40a
NadyaGolub <nadyagolub@ukr.net>
2022-12-14 16:18:15 +0200

    Завдання 9 (після зауважень)

--- js/task-07.js
+++ js/task-07.js
@@ -5,5 +5,5 @@ const inputText = document.querySelector(`#text`);
 inputSize.addEventListener(`input`, inputRange);
 
 function inputRange() {
-    inputText.style.fontSize = `${ inputSize.value }px`;
+    inputText.style.fontSize = `${inputSize.value}px`;
 }
--- js/task-09.js
+++ js/task-09.js
@@ -6,6 +6,7 @@ const button = document.querySelector(`.change-color`);
 const color = document.querySelector(`.color`);
 
 button.addEventListener(`click`, () => {
-  document.body.style.backgroundColor = getRandomHexColor();
-  color.textContent = getRandomHexColor();
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  color.textContent = randomColor;
 })
